c09ef3252a844ca17e0111bed1e1c71b6244b943
Márton Elek <elek@apache.org>
2025-06-03 10:35:57 +0200

    jobq/stat: histogram support

--- pkg/jobq/stat.go
+++ pkg/jobq/stat.go
@@ -12,8 +12,10 @@ import (
 )
 
 type Stat struct {
-	Server   string `required:"true"`
-	Identity string `required:"true"`
+	Server        string `required:"true"`
+	Identity      string `required:"true"`
+	WithHistogram bool
+	Placement     *int
 }
 
 func (s *Stat) Run() error {
@@ -52,17 +54,34 @@ func (s *Stat) Run() error {
 	client := jobq.WrapConn(conn)
 	defer client.Close()
 
-	stats, err := client.StatAll(ctx, false)
-	if err != nil {
-		return errors.WithStack(err)
+	var stats []jobq.QueueStat
+	if s.Placement == nil {
+		results, err := client.StatAll(ctx, s.WithHistogram)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		stats = append(stats, results...)
+	} else {
+		result, err := client.Stat(ctx, storj.PlacementConstraint(*s.Placement), s.WithHistogram)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		stats = append(stats, result)
 	}
+
 	for _, stat := range stats {
 		fmt.Println("Placement", stat.Placement)
 		fmt.Println("Count", stat.Count)
 		fmt.Println("MaxSegmentHealth", stat.MaxSegmentHealth)
 		fmt.Println("MinSegmentHealth", stat.MinSegmentHealth)
 		fmt.Println()
-
+		if s.WithHistogram {
+			fmt.Println("Histogram:")
+			for _, bucket := range stat.Histogram {
+				fmt.Printf("  missing: %d - oop: %d: %d\n", bucket.NumMissing, bucket.NumOutOfPlacement, bucket.Count)
+			}
+			fmt.Println()
+		}
 	}
 	return nil
 
