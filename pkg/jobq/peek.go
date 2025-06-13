d98435bc7dbdfdafd80f35307280306f7a24ff0e
Márton Elek <elek@apache.org>
2025-06-13 10:27:28 +0200

    jobq/peek: placement support + bumped interface usage

--- pkg/jobq/peek.go
+++ pkg/jobq/peek.go
@@ -54,12 +54,16 @@ func (s *Peek) Run() error {
 	client := jobq.WrapConn(conn)
 	defer client.Close()
 
-	peek, err := client.Peek(ctx, 10, nil, nil)
+	var included []storj.PlacementConstraint
+	if s.Placement != nil {
+		included = []storj.PlacementConstraint{storj.PlacementConstraint(*s.Placement)}
+	}
+	peek, err := client.Peek(ctx, 10, included, nil)
 	if err != nil {
 		return errors.WithStack(err)
 	}
 	for _, job := range peek {
-		fmt.Printf("Job ID: %s/%d, Missing: %d, OOP: %d, Helath: %f\n", job.ID.StreamID, job.ID.Position, job.NumMissing, job.NumOutOfPlacement, job.Health)
+		fmt.Printf("Job ID: %s/%d, Healthy: %d, Retrievable: %d, OOP: %d, Health: %f\n", job.ID.StreamID, job.ID.Position, job.NumNormalizedHealthy, job.NumNormalizedRetrievable, job.NumOutOfPlacement, job.Health)
 	}
 	return nil
 }
