6928bd999bfcfa452b55cb9bbd2a368b3c1d904f
Márton Elek <elek@apache.org>
2025-09-11 15:37:36 +0200

    jobq/peek: configurable limit + placement display

--- pkg/jobq/peek.go
+++ pkg/jobq/peek.go
@@ -3,8 +3,9 @@ package jobq
 import (
 	"context"
 	"fmt"
-	"github.com/pkg/errors"
 	"path/filepath"
+
+	"github.com/pkg/errors"
 	"storj.io/common/identity"
 	"storj.io/common/peertls/tlsopts"
 	"storj.io/common/storj"
@@ -16,6 +17,7 @@ type Peek struct {
 	Identity      string `required:"true"`
 	WithHistogram bool
 	Placement     *int
+	Number        int `help:"number of jobs to peek" alias:"n" default:"10"`
 }
 
 func (s *Peek) Run() error {
@@ -58,12 +60,12 @@ func (s *Peek) Run() error {
 	if s.Placement != nil {
 		included = []storj.PlacementConstraint{storj.PlacementConstraint(*s.Placement)}
 	}
-	peek, err := client.Peek(ctx, 10, included, nil)
+	peek, err := client.Peek(ctx, s.Number, included, nil)
 	if err != nil {
 		return errors.WithStack(err)
 	}
 	for _, job := range peek {
-		fmt.Printf("Job ID: %s/%d, Healthy: %d, Retrievable: %d, OOP: %d, Health: %f\n", job.ID.StreamID, job.ID.Position, job.NumNormalizedHealthy, job.NumNormalizedRetrievable, job.NumOutOfPlacement, job.Health)
+		fmt.Printf("Job ID: %s/%d, Healthy: %d, Retrievable: %d, OOP: %d, Health: %f, Placement: %d\n", job.ID.StreamID, job.ID.Position, job.NumNormalizedHealthy, job.NumNormalizedRetrievable, job.NumOutOfPlacement, job.Health, job.Placement)
 	}
 	return nil
 }
