19d02f9e099a8a9311db705d854be9976f0f3d8a
Márton Elek <elek@apache.org>
2025-09-24 14:09:02 +0200

    load/piece-upload: use cached connection

--- pkg/load/piece_upload.go
+++ pkg/load/piece_upload.go
@@ -4,15 +4,15 @@ import (
 	"bytes"
 	"context"
 	crand "crypto/rand"
+	"io"
+	"time"
+
 	"github.com/elek/stbb/pkg/util"
 	"github.com/pkg/errors"
-	"io"
 	"storj.io/common/memory"
 	"storj.io/common/pb"
-	"storj.io/common/rpc"
 	"storj.io/common/storj"
 	"storj.io/uplink/private/piecestore"
-	"time"
 )
 
 type PieceUpload struct {
@@ -40,22 +40,22 @@ func (p *PieceUpload) Run() error {
 		return errors.WithStack(err)
 	}
 
-	p.RunTest(func(ctx context.Context, piece storj.PieceID) error {
-		return p.connectAndUpload(ctx, dialer, piece, data)
-	})
-	return nil
-}
-
-func (p *PieceUpload) connectAndUpload(ctx context.Context, d rpc.Dialer, pieceID storj.PieceID, data []byte) (err error) {
-	client, err := piecestore.Dial(ctx, d, p.NodeURL, piecestore.DefaultConfig)
+	client, err := piecestore.Dial(context.Background(), dialer, p.NodeURL, piecestore.DefaultConfig)
 	if err != nil {
 		return errors.WithStack(err)
 	}
-	client.UploadHashAlgo = pb.PieceHashAlgorithm(pb.PieceHashAlgorithm_BLAKE3)
+	client.UploadHashAlgo = pb.PieceHashAlgorithm_BLAKE3
 	defer func() {
 		_ = client.Close()
 	}()
 
+	p.RunTest(func(ctx context.Context, piece storj.PieceID) error {
+		return p.connectAndUpload(ctx, client, piece, data)
+	})
+	return nil
+}
+
+func (p *PieceUpload) connectAndUpload(ctx context.Context, client *piecestore.Client, pieceID storj.PieceID, data []byte) (err error) {
 	limit, privateKey, _, err := p.CreateOrderLimit(ctx, pieceID, int64(len(data)), p.NodeURL.ID)
 	_, err = client.UploadReader(ctx, limit, privateKey, SlowReader{
 		pause:    p.Slow,
