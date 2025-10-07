f9c9579c8e349ae86e2f47044cf1ed6d8f1a38f1
Márton Elek <elek@apache.org>
2025-10-07 12:05:43 +0200

    admin: helper to update user (password/status)

--- pkg/admin/admin.go
+++ pkg/admin/admin.go
@@ -2,4 +2,5 @@ package admin
 
 type Admin struct {
 	SetBucketPlacement SetBucketPlacement `cmd:"" help:"set the default placement of a bucket"`
+	UpdateUser         UpdateUser         `cmd:"" help:"update some fields of a user"`
 }
---
+++ pkg/admin/update_user.go
@@ -0,0 +1,56 @@
+package admin
+
+import (
+	"context"
+
+	"github.com/elek/stbb/pkg/db"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+	"golang.org/x/crypto/bcrypt"
+	"storj.io/storj/satellite/console"
+)
+
+type UpdateUser struct {
+	db.WithDatabase
+	Email    string `arg:""`
+	Password string
+	Status   *int `name:"status" help:"set user status"`
+}
+
+func (s *UpdateUser) Run() error {
+	ctx := context.Background()
+
+	log, err := zap.NewDevelopment()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	satelliteDB, err := s.WithDatabase.GetSatelliteDB(ctx, log)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	user, err := satelliteDB.Console().Users().GetByEmail(ctx, s.Email)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	request := console.UpdateUserRequest{}
+	if s.Password != "" {
+		raw, err := bcrypt.GenerateFromPassword([]byte(s.Password), 4)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		request.PasswordHash = raw
+	}
+	if s.Status != nil {
+		status := console.UserStatus(*s.Status)
+		request.Status = &status
+	}
+
+	err = satelliteDB.Console().Users().Update(ctx, user.ID, request)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
