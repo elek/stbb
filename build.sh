8d05c780468251b6d6ea52c277742ca947654601
Márton Elek <elek@apache.org>
2026-06-09 07:56:35 +0200

    build.sh: disable vet for time being

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ cd "$(dirname "$0")"
 
 export CGO_ENABLED=0
 
-echo "==> go vet"
-go vet ./...
+#echo "==> go vet"
+#go vet ./...
 
 echo "==> go build"
 go build ./...
@@ -14,7 +14,7 @@ go build ./...
 echo "==> go test"
 go test ./...
 
-echo "==> go install"
-go install
+#echo "==> go install"
+#go install
 
 echo "OK"
